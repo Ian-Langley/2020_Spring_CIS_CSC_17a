1c9252d7912c7165396bcacfcb35351edd120d34
Ian-Langley <60118887+Ian-Langley@users.noreply.github.com>
2020-02-23 17:26:27 -0800

    IntroLab

---
+++ Class/IntroLab/HelloWorld/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Ian Langley
+ * 
+ * Introductory program used to show the download and useability of NetBeans IDE 8.2
+ *
+ * Created on February 23, 2020, 5:22 PM
+ */
+
+#include <cstdlib>
+#include<iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+    cout << "Hello World";
+    return 0;
+}
+
